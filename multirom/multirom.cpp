f1db097ea4512b7f8e629199f7333c6db1e315a8
nkk71 <nkk71x@gmail.com>
2016-09-09 23:17:13 +0300

    MultiROM: fix compile warning mtime
    Change-Id: I900b8a23b48bb7033756a744a29287a275d33159

--- multirom/multirom.cpp
+++ multirom/multirom.cpp
@@ -2676,7 +2676,7 @@ void MultiROM::executeCacheScripts()
 	struct dirent *dt;
 	struct stat info;
 	struct script_t  {
-		unsigned long mtime;
+		time_t mtime; // unsigned long mtime;
 		std::string name;
 		int type;
 	} script;
