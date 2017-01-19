NamelessRom/android_bootable_recovery-twrp

gui: Actions: Toggle backlight on power key
Create GUIAction to handle KEY_POWER with a screen backlight toggle.

Change-Id: Iad0a7923b4a776e0336722db74d6fc46cd0107a2

gui/objects.hpp
@@ -366,6 +366,7 @@ protected:
 	int checkpartitionlifetimewrites(std::string arg);
 	int mountsystemtoggle(std::string arg);
 	int setlanguage(std::string arg);
+	int setbacklight(std::string arg);
 	int twcmd(std::string arg);
 
 	int simulate;

jollaman999/Team-Win-Recovery-Project

gui/objects.hpp
@@ -402,6 +402,7 @@ protected:
 	int system_image_upgrader(std::string arg);
 #endif //TARGET_RECOVERY_IS_MULTIROM
 	int setlanguage(std::string arg);
+	int togglebacklight(std::string arg);
 	int twcmd(std::string arg);
 
 	int simulate;