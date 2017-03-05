SHRP/shrp_bootable_recovery

twrpDigest: don't crash when zip.md5 file is empty
Change-Id: I14145b32c7993bb0cd236b2982ed00bc2c791282

twrpDigest.cpp
@@ -154,7 +154,7 @@ int twrpDigest::verify_md5digest(void) {
 		snprintf(hex, 3, "%02x", md5sum[i]);
 		md5str += hex;
 	}
-	if (tokens.at(0) != md5str)
+	if (tokens.empty() || tokens.at(0) != md5str)
 		return MD5_MATCH_FAIL;
 
 	return MD5_OK;

jollaman999/Team-Win-Recovery-Project

twrpDigest.cpp
@@ -128,7 +128,7 @@ int twrpDigest::verify_md5digest(void) {
 		snprintf(hex, 3, "%02x", md5sum[i]);
 		md5string += hex;
 	}
-	if (tokens.at(0) != md5string)
+	if (tokens.empty() || tokens.at(0) != md5string)
 		return MD5_MATCH_FAIL;
 
 	return MD5_OK;